12551cf6fd4a13facc90431e50787330144ce4a5
zurawskigabriel <gabrielzds03@gmail.com>
2022-11-25 18:12:17 -0300

    inicio da classe de cartas

--- src/main/java/poo/modelo/CardPokemon.java
+++ src/main/java/poo/modelo/CardPokemon.java
@@ -2,8 +2,27 @@ package poo.modelo;
 
 public class CardPokemon extends Card {
     private int hp;
-    //private String tipo;
-    //private Strign fraqueza;
+    private int ataque;
+    private int custoRecuo;
+    private String nome;
+    private String fraqueza;
+
+    private enum tipo {
+        AGUA, FOGO, ELETRICO, PSIQUICO, LUTADOR, SOMBRIO, ACO, FADA, DRAGAO, INCOLOR
+    }
+
+    private enum categoria {
+        BASICO, ESTAGIO1, ESTAGIO2, VMAX
+    }
+
+    private enum fraqueza {
+
+    }
+
+    private enum resistencia {
+
+    }
+
     public CardPokemon(String anId, String anImageId, int val, int anHp) {
         super(anId, anImageId, val);
         hp = anHp;
